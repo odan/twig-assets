3164daa590884ef1233573f22b39458b1f9f8284
Maciej Fikus <maciej.fikus@wakacje.pl>
2020-07-31 12:16:07 +0200

    Add possibility to insert custom attributes and override existing while redering html element.

--- tests/TwigAssetsExtensionTest.php
+++ tests/TwigAssetsExtensionTest.php
@@ -108,6 +108,35 @@ class TwigAssetsExtensionTest extends AbstractTest
      *
      * @return void
      */
+    public function testJsWithCustomAttributes()
+    {
+        $file = vfsStream::newFile('public/test.js')->at($this->root)->setContent('alert(3);');
+        $realFileUrl = $file->url();
+        $filename = '@public/test.js';
+
+        $actual = $this->extension->assets(
+            [
+                'files' => [$filename],
+                'attributes' => [
+                    'type' => 'application/javascript'
+                ],
+                'inline' => false
+            ]
+        );
+
+        $dom = new \DOMDocument();
+        $dom->loadHTML($actual);
+        $element = $dom->getElementsByTagName('script')->item(0);
+
+        // new or overwritten attribute
+        $this->assertSame('application/javascript', $element->getAttribute('type'));
+    }
+
+    /**
+     * Test.
+     *
+     * @return void
+     */
     public function testCssDefault()
     {
         $content = 'body {
@@ -127,9 +156,52 @@ class TwigAssetsExtensionTest extends AbstractTest
         $actual2 = $this->extension->assets(['files' => [$filename], 'inline' => false]);
         $this->assertRegExp($this->styleInlineFileRegex, $actual2);
 
-        // update js file, cache must be rebuild
+        // update css file, cache must be rebuild
         file_put_contents($filename, 'alert(4);');
         $actual3 = $this->extension->assets(['files' => [$filename], 'inline' => false]);
         $this->assertRegExp($this->styleInlineFileRegex, $actual3);
+    }
+
+    /**
+     * Test.
+     *
+     * @return void
+     */
+    public function testCssWithCustomAttributes()
+    {
+        $content = 'body {
+            /* background-color: #F4F4F4; */
+            background-color: #f9fafa;
+            /* background-color: #f8f8f8; */
+            /* 60px to make the container go all the way to the bottom of the topbar */
+            padding-top: 60px;
+        }';
+
+        $file = vfsStream::newFile('test.css')->at($this->root)->setContent($content);
+        $filename = $file->url();
+
+        $actual = $this->extension->assets(
+            [
+                'files'  => [$filename],
+                'attributes' => [
+                    'rel' => 'preload',
+                    'as' => 'style',
+                    'onload' => 'this.onload=null;this.rel=\'stylesheet\''
+                ],
+                'inline' => false,
+            ]
+        );
+
+        $dom = new \DOMDocument();
+        $dom->loadHTML($actual);
+        $element = $dom->getElementsByTagName('link')->item(0);
+
+        // new or overwritten attribute
+        $this->assertSame('preload', $element->getAttribute('rel'));
+        $this->assertSame('style', $element->getAttribute('as'));
+        $this->assertSame('this.onload=null;this.rel=\'stylesheet\'', $element->getAttribute('onload'));
+
+        //untouched default attribute
+        $this->assertSame('all', $element->getAttribute('media'));
     }
 }
